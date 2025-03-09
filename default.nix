d3c3111a774fc45ffc4bdea75eda4c29852cb55f
Stefan <git@stefn.co.uk>
2025-03-09 21:28:38 +0000

    Fix hash

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 (import
   (fetchTarball {
     url = "https://github.com/edolstra/flake-compat/archive/v1.1.0.tar.gz";
-    sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
+    sha256 = "19d2z6xsvpxm184m41qrpi1bplilwipgnzv9jy17fgw4";
   })
   {
     src = ./.;
