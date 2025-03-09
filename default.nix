32daecb4cfa6679ba1eed987942f151b441d75d4
Stefan <git@stefn.co.uk>
2025-03-09 21:34:37 +0000

    Fancier default.nix

--- default.nix
+++ default.nix
@@ -1,9 +1,38 @@
-(import
-  (fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/v1.1.0.tar.gz";
-    sha256 = "19d2z6xsvpxm184m41qrpi1bplilwipgnzv9jy17fgw421785q1m";
-  })
-  {
-    src = ./.;
-  }
-).defaultNix.packages.${builtins.currentSystem}.build
+let
+  pkgs = import <nixpkgs> { };
+  flake =
+    (import
+      (fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+        sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
+      })
+      {
+        src = ./.;
+      }
+    ).defaultNix;
+  buildScript = flake.packages.${builtins.currentSystem}.build;
+in
+pkgs.stdenv.mkDerivation {
+  name = "sallys-bakes-site";
+
+  src = ./.;
+
+  buildInputs = with pkgs; [
+    buildScript
+    jekyll
+  ];
+
+  buildPhase = ''
+    # Ensure we're in the source directory
+    cd $src
+
+    # Run the Jekyll build
+    ${buildScript}/bin/build
+  '';
+
+  installPhase = ''
+    # Copy the built site to the output directory
+    mkdir -p $out
+    cp -r _site/* $out/
+  '';
+}
